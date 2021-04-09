87b8e21fe162c05cefb6039eaec635ecc8983f96
Sowjanya-0 <78260137+Sowjanya-0@users.noreply.github.com>
2021-04-09 23:31:41 +0530

    Add files via upload

---
+++ Experiment14/Queue.py
@@ -0,0 +1,56 @@
+#Initial Queue
+queue = []
+
+# EnQueue
+def enqueue(item):
+    queue.append(item)
+    
+# Stack Display
+def display():
+    print(queue)
+
+# DeQueue  
+def dequeue():
+    print(queue.pop(0))
+
+#Queue Searching Operation
+def search(key):
+    flag =0
+    for i in range (0,len(queue)) :
+        if (queue[i]==key):
+            print("\nFound at index "+str(i))
+        else:
+            flag=flag +1
+        if (flag==len(queue) ):
+            print("\nKey not present")
+            
+    
+print("Menu Option")
+print("1.Enqueue 2.Dequeue 3.Search 4.Display 5.Exit")
+n = int(input("please enter a choice: "))
+while(n>0):
+    if n==1: 
+        e=str(input("Enter any alphabet: "))
+        enqueue(e)
+        print("1.Enqueue 2.Dequeue 3.Search 4.Display 5.Exit")
+        n = int(input("please enter a choice: "))
+    elif n==2: 
+        dequeue()
+        print("1.Enqueue 2.Dequeue 3.Search 4.Display 5.Exit")
+        n = int(input("please enter a choice: "))
+    elif n==3:
+        k = str(input("Enter a key: "))
+        search(k)
+        print("1.Enqueue 2.Dequeue 3.Search 4.Display 5.Exit")
+        n = int(input("please enter a choice: "))
+    elif n==4:
+        display()
+        print("1.Enqueue 2.Dequeue 3.Search 4.Display 5.Exit")
+        n = int(input("please enter a choice: "))
+    elif n==5:
+        exit()
+    else: 
+        print("Enter 1-4 only")
+        print("1.Enqueue 2.Dequeue 3.Search 4.Display 5.Exit")
+        n = int(input("please enter a choice: "))
+    
